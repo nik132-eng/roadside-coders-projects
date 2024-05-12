6b54559151986384e73f4c9c0565262e69b8eaa7
Nikunj Rohit <nikunjrohit10@gmail.com>
2024-05-13 01:13:51 +0530

    add pagination logic

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -19,16 +19,25 @@ const fetchData = async (): Promise<Product[]> => {
 
 const Pagination = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [pageNumber, setPageNumber] = useState<number>(1); 
 
   useEffect(() => {
     fetchData().then((products) => setProducts(products));
-  }, []);
+  }, [pageNumber]);
+
+  function pageHandler(page: number): void {
+    console.log(page, "page");
+    const totalPages = Math.ceil(products.length / 10); 
+    if (page > 0 && page <= totalPages) { 
+      setPageNumber(page);
+    }
+  }
 
   return (
     <div>
       {products.length > 0 && (
         <div className={styles.products}> 
-          {products.map((prod) => (
+          {products.slice(pageNumber * 10 -10 ,pageNumber * 10).map((prod) => (
             <span className={styles.products__single} key={prod.id}>
               <img src={prod.thumbnail} alt={prod.title} />
               <span>{prod.title}</span>
@@ -36,6 +45,12 @@ const Pagination = () => {
           ))}
         </div>
       )}
+        <div className={styles.pagination}>
+            
+        <span onClick={()=> pageHandler(pageNumber - 1)}>👈🏻</span>
+        <span>{pageNumber}</span>
+        <span onClick={()=> pageHandler(pageNumber + 1)}>👉🏻</span>
+        </div>
     </div>
   );
 };
